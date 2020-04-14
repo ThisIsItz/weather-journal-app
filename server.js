dd5ffed63aeb7c7ccd7c79024709dea7d83364bf
ThisIsItz <itziarurbieta94@gmail.com>
2020-04-14 11:40:06 +0100

    Deleting OnlyComments folder

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+const projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
+const bodyParser = require('body-parser');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +14,32 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+ const port = 8000;
+ const server = app.listen(port, listening);
+
+  // Callback to debug
+ function listening() {
+     console.log(`server running on localhost: ${port}`);
+ };
+
+// Callback function to complete GET '/all'
+app.get('/weatherData', function(req, res) {
+    res.send(projectData);
+})
+
+// Post Route
+app.post('/addWeather', function (req, res) {
+    projectData = req.body;
+    console.log('post request: received');
+    console.log(projectData);
+    res.send('Post received');
+});
+
+
